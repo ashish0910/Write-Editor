1963495520c8eca7b39d7fd575a7ae4e17c0d81b
ashish0910 <ashish09101998@gmail.com>
2019-05-30 11:46:04 +0530

    palette setting up

--- Write.activity/js/activity.js
+++ Write.activity/js/activity.js
@@ -1,4 +1,12 @@
-define(["sugar-web/activity/activity"], function (activity) {
+define([
+	"sugar-web/activity/activity",
+    "sugar-web/env",
+    "sugar-web/graphics/icon",
+    "webL10n",
+    "sugar-web/graphics/presencepalette",
+	"activity/palettes/format-text-palette",
+	"activity/palettes/edit-text-palette"
+], function (activity, env, icon, webL10n, presencepalette, toolpalette) {
 
 	// Manipulate the DOM only when it is ready.
 	requirejs(['domReady!'], function (doc) {
